2b9c1230de7bd372b2862ef33432767363b2c60b
miich <mohammed.iich@insa-lyon.fr>
2026-03-03 09:53:53 +0100

    Exercices 4.2 compilés: ret42.c, main.s, assemblage et linkage

---
+++ compiler/generated/ifccParser.h
@@ -0,0 +1,93 @@
+
+// Generated from ifcc.g4 by ANTLR 4.13.2
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+
+
+class  ifccParser : public antlr4::Parser {
+public:
+  enum {
+    T__0 = 1, T__1 = 2, T__2 = 3, T__3 = 4, T__4 = 5, T__5 = 6, T__6 = 7, 
+    RETURN = 8, CONST = 9, COMMENT = 10, DIRECTIVE = 11, WS = 12
+  };
+
+  enum {
+    RuleAxiom = 0, RuleProg = 1, RuleReturn_stmt = 2
+  };
+
+  explicit ifccParser(antlr4::TokenStream *input);
+
+  ifccParser(antlr4::TokenStream *input, const antlr4::atn::ParserATNSimulatorOptions &options);
+
+  ~ifccParser() override;
+
+  std::string getGrammarFileName() const override;
+
+  const antlr4::atn::ATN& getATN() const override;
+
+  const std::vector<std::string>& getRuleNames() const override;
+
+  const antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+  antlr4::atn::SerializedATNView getSerializedATN() const override;
+
+
+  class AxiomContext;
+  class ProgContext;
+  class Return_stmtContext; 
+
+  class  AxiomContext : public antlr4::ParserRuleContext {
+  public:
+    AxiomContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    ProgContext *prog();
+    antlr4::tree::TerminalNode *EOF();
+
+
+    virtual std::any accept(antlr4::tree::ParseTreeVisitor *visitor) override;
+   
+  };
+
+  AxiomContext* axiom();
+
+  class  ProgContext : public antlr4::ParserRuleContext {
+  public:
+    ProgContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    Return_stmtContext *return_stmt();
+
+
+    virtual std::any accept(antlr4::tree::ParseTreeVisitor *visitor) override;
+   
+  };
+
+  ProgContext* prog();
+
+  class  Return_stmtContext : public antlr4::ParserRuleContext {
+  public:
+    Return_stmtContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    antlr4::tree::TerminalNode *RETURN();
+    antlr4::tree::TerminalNode *CONST();
+
+
+    virtual std::any accept(antlr4::tree::ParseTreeVisitor *visitor) override;
+   
+  };
+
+  Return_stmtContext* return_stmt();
+
+
+  // By default the static state used to implement the parser is lazily initialized during the first
+  // call to the constructor. You can call this function if you wish to initialize the static state
+  // ahead of time.
+  static void initialize();
+
+private:
+};
+
